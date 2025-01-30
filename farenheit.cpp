b83198f3f9fc9456f2dc1d2bc5a2ff5792c3cea3
SakshiSupekar07 <supekarsakshj07@gmail.com>
2025-01-30 08:53:11 +0530

    Add files via upload

---
+++ farenheit.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float celcius,farenheit;
+    cout<<"Enter temperature in celcius"<<endl;
+
+    cin>>celcius;
+    farenheit=1.8*celcius+32;
+    cout<<"Temeprature in farenheit is " <<farenheit<<endl;
+    return 0;
+    }
